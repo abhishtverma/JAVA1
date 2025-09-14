3f6ea06ed5eef27e17e25caa938a063722fa5cbc
abhishtverma <abhishtverma555@gmail.com>
2025-09-14 22:23:38 +0530

    save

--- OOPs/Abstraction/Flyable2.java
+++ OOPs/Abstraction/Flyable2.java
@@ -1,6 +1,6 @@
 package OOPs.Abstraction;
 
-// this is a interface class(this is like giving necessary propery to inherit other than parent class)
+// this is a interface class(this is like giving necessary property to inherit other than parent class)
 public interface Flyable2 {
     abstract void fly(); // abstract method
 }
